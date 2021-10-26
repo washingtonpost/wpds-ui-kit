51536a0063aeb37110ecca755cab14ef7311c701
artmsilva <artmsilva@gmail.com>
2021-10-26 10:46:31 -0400

    oops fix SSR render

---
+++ website/pages/_document.js
@@ -0,0 +1,27 @@
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "@washingtonpost/ui-theme";
+
+export default class Document extends NextDocument {
+	static async getInitialProps(ctx) {
+		const initialProps = await NextDocument.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<style
+						id="stitches"
+						dangerouslySetInnerHTML={{ __html: getCssText() }}
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
