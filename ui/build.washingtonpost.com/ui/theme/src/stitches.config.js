deed94e622fb0c8f268856bb3e4c92aac2bbf082
artmsilva <artmsilva@gmail.com>
2021-11-18 11:28:00 -0500

    chore: comment out dark mode shit

--- ui/build.washingtonpost.com/ui/theme/src/stitches.config.js
+++ ui/build.washingtonpost.com/ui/theme/src/stitches.config.js
@@ -148,23 +148,23 @@ export const globalStyles = globalCss({
 			src: "url(https://www.washingtonpost.com/wp-stat/assets/fonts/ITC_Franklin-Light.woff2)",
 		},
 	],
-	"@dark": {
-		":root:not(.light)": {
-			...Object.keys(darkTheme.colors).reduce(
-				(varSet, currentColorKey) => {
-					const currentColor = darkTheme.colors[currentColorKey];
-					const currentColorValue =
-						currentColor.value.substring(0, 1) === "$"
-							? `$colors${currentColor.value}`
-							: currentColor.value;
+	// "@dark": {
+	// 	":root:not(.light)": {
+	// 		...Object.keys(darkTheme.colors).reduce(
+	// 			(varSet, currentColorKey) => {
+	// 				const currentColor = darkTheme.colors[currentColorKey];
+	// 				const currentColorValue =
+	// 					currentColor.value.substring(0, 1) === "$"
+	// 						? `$colors${currentColor.value}`
+	// 						: currentColor.value;
 
-					return {
-						[currentColor.variable]: currentColorValue,
-						...varSet,
-					};
-				},
-				{}
-			),
-		},
-	},
+	// 				return {
+	// 					[currentColor.variable]: currentColorValue,
+	// 					...varSet,
+	// 				};
+	// 			},
+	// 			{}
+	// 		),
+	// 	},
+	// },
 });
