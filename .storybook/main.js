3f27f90551c3c136e6a796703123e6a37f3170e8
artmsilva <artmsilva@gmail.com>
2021-11-19 13:53:05 -0500

    feat: add icon component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,5 @@
 module.exports = {
 	stories: ["../ui/**/*.stories.@(ts|tsx)"],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"storybook-addon-playroom",
-		"@storybook/addon-a11y",
-		"storybook-dark-mode",
-	],
-	webpackFinal: async (config, { configType }) => {
-		if (process.env.NODE_ENV === "production") {
-			config.output.publicPath = "/storybook/";
-		}
-		return config;
-	},
-	managerWebpack: async (config) => {
-		if (process.env.NODE_ENV === "production") {
-			config.output.publicPath = "/storybook/";
-		}
-		return config;
-	},
+	addons: ["@storybook/addon-essentials"],
 	previewBody: (body) => `${body}`,
 };
