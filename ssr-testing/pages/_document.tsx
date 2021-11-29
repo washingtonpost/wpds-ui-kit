d92a14d03edbe21d9ed5f5f133d603b038d62eab
artmsilva <artmsilva@gmail.com>
2021-11-29 11:01:55 -0500

    ssr-testing use kit

--- ssr-testing/pages/_document.tsx
+++ ssr-testing/pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { getCssText } from "@washingtonpost/ui-theme";
+import { getCssText } from "@washingtonpost/wpds-ui-kit";
 
 export default class Document extends NextDocument {
 	static async getInitialProps(ctx) {
