722532da32abe32cba3fbc8bd7a7fc0629b6d6d1
artmsilva <artmsilva@gmail.com>
2021-12-29 15:26:55 -0500

    fix: adds dark mode to storybook and use unified lerna mode

--- ui/build.washingtonpost.com/.storybook/preview.js
+++ ui/build.washingtonpost.com/.storybook/preview.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { globalStyles } from "@washingtonpost/wpds-ui-kit";
+import React, { useEffect } from "react";
+import { globalStyles, darkTheme } from "@washingtonpost/wpds-ui-kit";
+// import { useDarkMode } from "storybook-dark-mode";
+
+// Detect if users preferred color scheme is dark
+const isDark =
+	typeof window !== `undefined`
+		? window.matchMedia &&
+		  window.matchMedia("(prefers-color-scheme: dark)").matches
+		: null;
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,9 +21,26 @@ export const parameters = {
 		element: "#root",
 		manual: false,
 	},
+	darkMode: {
+		current: isDark ? "dark" : "light",
+		stylePreview: true,
+		darkClass: darkTheme.className,
+	},
 };
 
 function GlobalStyles(props) {
+	// const darkModeActive = useDarkMode();
+
+	// add darkTheme.className to body if darkMode is active and darkTheme.className exists using a useEffect hook
+
+	// useEffect(() => {
+	// 	if (darkModeActive && darkTheme.className) {
+	// 		document.body.classList.add(darkTheme.className);
+	// 	} else {
+	// 		document.body.classList.remove(darkTheme.className);
+	// 	}
+	// }, [darkModeActive]);
+
 	globalStyles();
 	return <div>{props.children}</div>;
 }
