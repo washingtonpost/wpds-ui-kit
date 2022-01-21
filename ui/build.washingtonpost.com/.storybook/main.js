7529c3d3c7e0def52b43abd166574463619a5cbd
artmsilva <artmsilva@gmail.com>
2022-01-21 12:54:45 -0500

    so cvery close

--- ui/build.washingtonpost.com/.storybook/main.js
+++ ui/build.washingtonpost.com/.storybook/main.js
@@ -1,35 +1,38 @@
+// add process.env
+require("dotenv").config();
+
 module.exports = {
-	stories: ["../ui/**/*.stories.@(ts|tsx)"],
-	addons: [
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		"storybook-dark-mode",
-	],
-	previewBody: (body) => `${body}`,
-	managerHead: (head) =>
-		`${head}${
-			process.env.VERCEL &&
-			`<script>
+  stories: ["../ui/**/*.stories.@(ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-dark-mode",
+  ],
+  previewBody: (body) => `${body}`,
+  managerHead: (head) =>
+    `${head}${
+      process.env.VERCEL &&
+      `<script>
     window['PREVIEW_URL'] = '/storybook/iframe.html';
 </script>`
-		}`,
-	webpackFinal: async (config, { configType }) => {
-		if (process.env.VERCEL) {
-			config.output.publicPath = "/storybook/";
-		}
-		return config;
-	},
-	managerWebpack: async (config) => {
-		if (process.env.VERCEL) {
-			config.output.publicPath = "/storybook/";
-		}
-		return config;
-	},
-	features: {
-		storyStoreV7: true,
-	},
-	framework: "@storybook/react",
-	core: {
-		builder: "webpack5",
-	},
+    }`,
+  webpackFinal: async (config, { configType }) => {
+    if (process.env.VERCEL) {
+      config.output.publicPath = "/storybook/";
+    }
+    return config;
+  },
+  managerWebpack: async (config) => {
+    if (process.env.VERCEL) {
+      config.output.publicPath = "/storybook/";
+    }
+    return config;
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
 };
