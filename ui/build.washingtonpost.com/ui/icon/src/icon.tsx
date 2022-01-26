834869c58f9a0eccecf3e05d9df1b9d352dc5717
artmsilva <artmsilva@gmail.com>
2022-01-26 10:02:36 -0500

    prettier formatting

--- ui/build.washingtonpost.com/ui/icon/src/icon.tsx
+++ ui/build.washingtonpost.com/ui/icon/src/icon.tsx
@@ -4,41 +4,41 @@ import * as VH from "@washingtonpost/wpds-visually-hidden";
 const NAME = "Icon";
 
 interface IconProps {
-	/**
-	 * size of the icon
-	 * @default '16'
-	 * @memberof IconProps
-	 * @example
-	 * <Icon size={16}><AddIcon />/Icon>
-	 * <Icon size={24}><AddIcon />/Icon>
-	 * <Icon size={32}><AddIcon />/Icon>
-	 */
-	size?: "16" | "24" | "32";
-	/**
-	 * The accessibility label for the icon. This label will be visually hidden but read aloud by VoiceOver and other screenreaders to describe the icon.
-	 * @type string
-	 * @memberof IconProps
-	 * @example
-	 * <Icon label="Add" size={16}><AddIcon /></Icon>
-	 */
-	label: string;
+  /**
+   * size of the icon
+   * @default '16'
+   * @memberof IconProps
+   * @example
+   * <Icon size={16}><AddIcon />/Icon>
+   * <Icon size={24}><AddIcon />/Icon>
+   * <Icon size={32}><AddIcon />/Icon>
+   */
+  size?: "16" | "24" | "32";
+  /**
+   * The accessibility label for the icon. This label will be visually hidden but read aloud by VoiceOver and other screenreaders to describe the icon.
+   * @type string
+   * @memberof IconProps
+   * @example
+   * <Icon label="Add" size={16}><AddIcon /></Icon>
+   */
+  label: string;
 }
 
 export const Icon: React.FC<IconProps> = ({ children, size = "16", label }) => {
-	const child = React.Children.only(children);
+  const child = React.Children.only(children);
 
-	return (
-		<>
-			{React.cloneElement(child as React.ReactElement, {
-				"aria-hidden": true,
-				focusable: false,
-				width: size,
-				height: size,
-				role: "img",
-			})}
-			<VH.VisuallyHidden>{label}</VH.VisuallyHidden>
-		</>
-	);
+  return (
+    <>
+      {React.cloneElement(child as React.ReactElement, {
+        "aria-hidden": true,
+        focusable: false,
+        width: size,
+        height: size,
+        role: "img",
+      })}
+      <VH.VisuallyHidden>{label}</VH.VisuallyHidden>
+    </>
+  );
 };
 
 Icon.displayName = NAME;
--- ui/build.washingtonpost.com/ui/visually-hidden/src/visually-hidden.tsx
+++ ui/build.washingtonpost.com/ui/visually-hidden/src/visually-hidden.tsx
@@ -1,33 +1,33 @@
 import * as Theme from "@washingtonpost/wpds-theme";
 
 export const VisuallyHidden = Theme.styled("span", {
-	position: "absolute",
-	border: 0,
-	padding: 0,
-	whiteSpace: "nowrap",
-	wordWrap: "normal",
-	width: 1,
-	height: 1,
-	margin: -1,
-	overflow: "hidden",
-	clip: "rect(0, 0, 0, 0)",
-	color: "currentColor",
+  position: "absolute",
+  border: 0,
+  padding: 0,
+  whiteSpace: "nowrap",
+  wordWrap: "normal",
+  width: 1,
+  height: 1,
+  margin: -1,
+  overflow: "hidden",
+  clip: "rect(0, 0, 0, 0)",
+  color: "currentColor",
 
-	// display label to screen when focused or active
-	"&:active": {
-		clip: "auto",
-		height: "auto",
-		width: "auto",
-		margin: 0,
-		overflow: "visible",
-		position: "static",
-	},
-	"&:focus": {
-		clip: "auto",
-		height: "auto",
-		width: "auto",
-		margin: 0,
-		overflow: "visible",
-		position: "static",
-	},
+  // display label to screen when focused or active
+  "&:active": {
+    clip: "auto",
+    height: "auto",
+    width: "auto",
+    margin: 0,
+    overflow: "visible",
+    position: "static",
+  },
+  "&:focus": {
+    clip: "auto",
+    height: "auto",
+    width: "auto",
+    margin: 0,
+    overflow: "visible",
+    position: "static",
+  },
 });
