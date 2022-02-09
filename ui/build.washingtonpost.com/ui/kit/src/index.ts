30f76be430b7180fcc04bf83aebcb4c5864d333c
artmsilva <artmsilva@gmail.com>
2022-02-09 09:55:33 -0500

    feat: container

--- ui/build.washingtonpost.com/ui/kit/src/index.ts
+++ ui/build.washingtonpost.com/ui/kit/src/index.ts
@@ -1,4 +1,5 @@
 // insert new component exports here
+export * from "@washingtonpost/wpds-container";
 export * from "@washingtonpost/wpds-theme";
 export * from "@washingtonpost/wpds-icon";
 export * from "@washingtonpost/wpds-visually-hidden";
