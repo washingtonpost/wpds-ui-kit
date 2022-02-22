7c3b5d7bb13597af718986ec674e6cd73d8e5620
artmsilva <artmsilva@gmail.com>
2022-02-22 12:14:09 -0500

    code out all the props

--- ui/build.washingtonpost.com/ui/checkbox/src/play.stories.tsx
+++ ui/build.washingtonpost.com/ui/checkbox/src/play.stories.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { styled } from "@washingtonpost/wpds-theme";
-
+import { Box } from "@washingtonpost/wpds-box";
 import { Checkbox as Component } from "./";
 
 export default {
   title: "Checkbox",
   component: Component,
   argTypes: {
+    size: {
+      options: ["087", "125"],
+    },
+    variant: {
+      options: ["primary", "secondary", "cta"],
+    },
+    style: {
+      options: ["outline", "fill"],
+    },
     defaultChecked: {
       options: [true, false, "indeterminate"],
     },
     checked: {
       options: [true, false, "indeterminate"],
+      defaultValue: true,
     },
     onCheckedChange: {
       action: "onCheckedChange",
@@ -41,19 +51,37 @@ const Template: ComponentStory<typeof Component> = (args) => (
 
 export const Play = Template.bind({});
 
-Play.args = {};
+Play.args = {
+  id: "boop",
+};
 
 const Decorator = styled("div", {
   display: "flex",
-  flexDirection: "column",
+  flexDirection: "row",
+  gap: "$050",
 });
 
 Play.decorators = [
   (Story) => (
     <Decorator>
       <Story />
+      <Box
+        as="label"
+        htmlFor="boop"
+        css={{
+          color: "$primary",
+          alignSelf: "flex-end",
+        }}
+      >
+        This is a label
+      </Box>
     </Decorator>
   ),
 ];
 
 Play.storyName = "Checkbox";
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
