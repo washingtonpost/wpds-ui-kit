968d4b3652f9667049759ba4b7e6792c98c85e86
Arturo Silva <arturo.silva@washpost.com>
2022-03-04 21:19:34 -0500

    feat: support fill attribute

--- ui/icon/src/icon.tsx
+++ ui/icon/src/icon.tsx
@@ -4,21 +4,31 @@ import { css, theme } from "@washingtonpost/wpds-theme";
 
 const NAME = "Icon";
 
-interface IconInterface extends React.HTMLAttributes<HTMLOrSVGImageElement> {
+interface IconInterface extends React.SVGProps<HTMLOrSVGElement> {
   size?: "100" | "150" | "200" | number;
   label: string;
   children?: React.ReactNode;
   className?: string;
 }
 
-export const Icon = React.forwardRef<HTMLOrSVGImageElement, IconInterface>(
-  ({ children, size = "100", label, className = "", ...props }, ref) => {
+export const Icon = React.forwardRef<React.ReactSVGElement, IconInterface>(
+  (
+    {
+      children,
+      size = "100",
+      fill = "currentColor",
+      label,
+      className = "",
+      ...props
+    },
+    ref
+  ) => {
     const child = React.Children.only(children);
 
     const IconSizeStyle = css({
       width: theme.sizes[size] || size,
       height: theme.sizes[size] || size,
-      fill: "currentColor",
+      fill,
     });
 
     return (
