e5f2c00a699ee796342d92fc9081c3afbe55b510
Maria Eguiluz <atomictangerine@users.noreply.github.com>
2022-08-30 18:01:22 -0400

    Feat: create accordion (#159)

---
+++ build.washingtonpost.com/snippets/accordion/accordion.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+/**
+ * See documentation for full component api
+ *
+```jsx
+import { Accordion } from "@washingtonpost/wpds-ui-kit";
+
+export default function Example() {
+  return (
+    <Accordion.Root type={ACCORDION_TYPE.single} defaultValue="item-1">
+      <Accordion.Item value={"item-1"}>
+        <Accordion.Trigger ref={myHeaderRef} density={ACCORDION_DENSITY.compact}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in
+          augue in felis pharetra finibus. In sagittis aliquam augue. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit.
+        </Accordion.Trigger>
+        <Accordion.Content ref={myContentRef}>
+          No! You don't even believe that! Gus has cameras everywhere, please.
+          Listen to yourself! No, he has known everything, all along. Where were
+          you today? In the lab? And you don't think it's possible that Tyrus
+          lifted the cigarette out of your locker? Come on! Don't you see? You
+          are the last piece of the puzzle. You are everything that he's wanted.
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item value={"item-2"}>
+        <Accordion.Trigger ref={myHeaderRef} density={ACCORDION_DENSITY.compact}>
+          How long will I have to social distance?
+        </Accordion.Trigger>
+        <Accordion.Content ref={myContentRef}>
+          <img
+            src="https://i.pravatar.cc/300"
+            alt="An avatar is an atomic component that represents an individual’s identity through a circular photo."
+          />
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  );
+}
+```
+ */
+export default function Example() {
+  return <div>See documentation for full example</div>;
+}
---
+++ ui/accordion/src/play.stories.jsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { Accordion, ACCORDION_DENSITY, ACCORDION_TYPE } from ".";
+
+export default {
+  title: "Accordion",
+  component: Accordion.Root,
+  subcomponents: {
+    Item: Accordion.Item,
+    Header: Accordion.Header,
+    Content: Accordion.Content,
+  },
+  argTypes: {
+    density: {
+      options: [
+        ACCORDION_DENSITY.compact,
+        ACCORDION_DENSITY.default,
+        ACCORDION_DENSITY.loose,
+      ],
+      control: "select",
+    },
+    type: {
+      options: [ACCORDION_TYPE.single, ACCORDION_TYPE.multiple],
+      control: "select",
+    },
+    disabled: {
+      options: [true, false],
+      control: "boolean",
+    },
+  },
+};
+
+const Template = (args) => {
+  const myHeaderRef = React.createRef();
+  const myContentRef = React.createRef();
+
+  return (
+    <Accordion.Root {...args}>
+      <Accordion.Item value={"item-1"}>
+        <Accordion.Trigger density={args.density} ref={myHeaderRef} {...args}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in
+          augue in felis pharetra finibus. In sagittis aliquam augue. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit.
+        </Accordion.Trigger>
+        <Accordion.Content ref={myContentRef}>
+          No! You don't even believe that! Gus has cameras everywhere, please.
+          Listen to yourself! No, he has known everything, all along. Where were
+          you today? In the lab? And you don't think it's possible that Tyrus
+          lifted the cigarette out of your locker? Come on! Don't you see? You
+          are the last piece of the puzzle. You are everything that he's wanted.
+        </Accordion.Content>
+      </Accordion.Item>
+      <Accordion.Item value={"item-2"}>
+        <Accordion.Trigger density={args.density} ref={myHeaderRef} {...args}>
+          How long will I have to social distance?
+        </Accordion.Trigger>
+        <Accordion.Content {...args} ref={myContentRef}>
+          <img
+            src="https://i.pravatar.cc/300"
+            alt="An avatar is an atomic component that represents an individual’s identity through a circular photo."
+          />
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  );
+};
+
+export const Play = Template.bind({});
+
+Play.args = {
+  density: ACCORDION_DENSITY.compact,
+  type: ACCORDION_TYPE.single,
+  defaultValue: "item-1",
+};
+
+Play.storyName = "Accordion";
