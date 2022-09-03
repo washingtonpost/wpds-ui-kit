c74324d2c6e17544a4f0ce3bc68a1137b5f8da3d
Arturo Silva <arturo.silva@washpost.com>
2022-09-02 23:13:07 -0400

    docs; update drawer to use propstable data

--- ui/drawer/src/index.ts
+++ ui/drawer/src/index.ts
@@ -1,1 +1,7 @@
 export * from "./Drawer";
+export { DrawerRoot } from "./DrawerRoot";
+export { DrawerContent } from "./DrawerContent";
+export { DrawerTrigger } from "./DrawerTrigger";
+export { DrawerClose } from "./DrawerClose";
+export { DrawerCustomTrigger } from "./DrawerCustomTrigger";
+export { DrawerScrim } from "./DrawerScrim";
