1c68bbf73f241f32925877f5c1b23f60f0e0faff
artmsilva <artmsilva@gmail.com>
2022-09-14 10:04:22 -0400

    fix: encourage use of ui-kit import and pkg

---
+++ build.washingtonpost.com/components/ComponentPage/ComponentDetails.jsx
@@ -0,0 +1,125 @@
+import * as React from "react";
+import { Box, theme } from "@washingtonpost/wpds-ui-kit";
+import CustomLink from "~/components/Typography/link";
+import CopyCodeButton from "~/components/Markdown/Components/CopyToClipBoard";
+
+export const ComponentDetails = ({ current, bundleSize, componentName }) => {
+  return (
+    <Box
+      css={{
+        marginBlockStart: "$100",
+        display: "flex",
+        rowGap: "$100",
+        flexDirection: "column",
+        fontFamily: "$meta",
+        fontSize: "$075",
+
+        pre: {
+          display: "inline",
+        },
+      }}
+    >
+      <Box
+        css={{
+          display: "flex",
+          fontWeight: "$bold",
+          textDecoration: "none",
+          color: "inherit",
+        }}
+      >
+        Bundle size:&nbsp;
+        <CustomLink
+          css={{
+            color: theme.colors.accessible,
+            textDecoration: "underline",
+            fontWeight: theme.fontWeights.regular,
+            "&:focus": {
+              outlineColor: "$signal",
+              outlineStyle: "solid",
+              outlineOffset: "2px",
+              outlineWidth: "2px",
+            },
+          }}
+          href={`https://bundlephobia.com/package/@washingtonpost/wpds-${current}`}
+          title={"Learn more about the bundle size at Bundlephobia.com"}
+        >
+          {bundleSize}
+        </CustomLink>
+      </Box>
+      <Box
+        css={{
+          fontWeight: "$bold",
+        }}
+      >
+        Install:{" "}
+        <pre>
+          <CopyCodeButton
+            as="code"
+            css={{
+              display: "inline",
+              fontWeight: "$light",
+              borderRadius: "$012",
+              backgroundColor: "$gray500",
+              color: "$accessible",
+              padding: "$025",
+            }}
+            textToCopy={`npm install @washingtonpost/wpds-ui-kit`}
+          >
+            npm install @washingtonpost/wpds-ui-kit
+          </CopyCodeButton>
+        </pre>
+      </Box>
+      <Box
+        css={{
+          fontWeight: "$bold",
+        }}
+      >
+        Usage:{" "}
+        <pre>
+          <CopyCodeButton
+            as="code"
+            css={{
+              display: "inline",
+              fontWeight: "$light",
+              borderRadius: "$012",
+              backgroundColor: "$gray500",
+              color: "$accessible",
+              padding: "$025",
+            }}
+            textToCopy={`import { ${componentName} } from "@washingtonpost/wpds-ui-kit";`}
+          >
+            import {"{"} {componentName} {"}"} from
+            &quot;@washingtonpost/wpds-ui-kit&quot;
+          </CopyCodeButton>
+        </pre>
+      </Box>
+      <Box
+        css={{
+          display: "flex",
+          fontWeight: "$bold",
+          textDecoration: "none",
+          color: "inherit",
+        }}
+      >
+        Source:&nbsp;
+        <CustomLink
+          css={{
+            color: theme.colors.accessible,
+            textDecoration: "underline",
+            fontWeight: theme.fontWeights.regular,
+            "&:focus": {
+              outlineColor: "$signal",
+              outlineStyle: "solid",
+              outlineOffset: "2px",
+              outlineWidth: "2px",
+            },
+          }}
+          href={`https://github.com/washingtonpost/wpds-ui-kit/tree/main/ui/${current}`}
+          title={"View on Github"}
+        >
+          View on Github
+        </CustomLink>
+      </Box>
+    </Box>
+  );
+};
