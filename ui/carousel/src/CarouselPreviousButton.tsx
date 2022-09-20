b8a9e8f12f1488092db274ff828703c47c885010
Andrew Berg <andrew.berg@washpost.com>
2022-09-20 11:13:40 -0400

    feat: added the asChild prop and Slots to previous and next buttons

--- ui/carousel/src/CarouselPreviousButton.tsx
+++ ui/carousel/src/CarouselPreviousButton.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
 import { Button } from "@washingtonpost/wpds-button";
 import { Icon } from "@washingtonpost/wpds-icon";
 import { ArrowLeft } from "@washingtonpost/wpds-assets/";
@@ -6,38 +7,68 @@ import { CarouselContext } from "./CarouselRoot";
 
 const NAME = "CarouselPreviousButton";
 
-export type CarouselPreviousButtonProps = React.ComponentPropsWithRef<
-  typeof Button
->;
+export type CarouselPreviousButtonProps = {
+  asChild?: boolean;
+} & React.ComponentPropsWithRef<typeof Button>;
 
 export const CarouselPreviousButton = React.forwardRef<
   HTMLButtonElement,
   CarouselPreviousButtonProps
->(({ onClick, css, ...props }, ref) => {
-  const { page, setPage, totalPages } = React.useContext(CarouselContext);
+>(
+  (
+    {
+      onClick,
+      css,
+      density = "compact",
+      icon = "center",
+      variant = "primary",
+      asChild,
+      ...props
+    },
+    ref
+  ) => {
+    const { page, setPage, totalPages } = React.useContext(CarouselContext);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (totalPages && page > 0) {
-      setPage(page - 1);
-      onClick && onClick(event);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (totalPages && page > 0) {
+        setPage(page - 1);
+        onClick && onClick(event);
+      }
+    };
+
+    let isDisabled;
+    if (totalPages) {
+      isDisabled = page === 0;
     }
-  };
 
-  return (
-    <Button
-      css={{ display: "inline-flex", ...css }}
-      onClick={handleClick}
-      density="compact"
-      icon="center"
-      variant="primary"
-      {...props}
-      ref={ref}
-    >
-      <Icon label="Previous" size="100">
-        <ArrowLeft />
-      </Icon>
-    </Button>
-  );
-});
+    return (
+      <>
+        {asChild ? (
+          <Slot
+            ref={ref}
+            onClick={handleClick}
+            disabled={isDisabled}
+            {...props}
+          />
+        ) : (
+          <Button
+            css={{ display: "inline-flex", ...css }}
+            onClick={handleClick}
+            density={density}
+            icon={icon}
+            variant={variant}
+            disabled={isDisabled}
+            {...props}
+            ref={ref}
+          >
+            <Icon label="Previous" size="100">
+              <ArrowLeft />
+            </Icon>
+          </Button>
+        )}
+      </>
+    );
+  }
+);
 
 CarouselPreviousButton.displayName = NAME;
