f346dbf5dac07c6d77909ad7cacd7997d63d09cf
atomictangerine <mbeguiluz@gmail.com>
2022-10-18 00:01:48 -0400

    chore: refactor

--- ui/select/src/play.stories.jsx
+++ ui/select/src/play.stories.jsx
@@ -14,31 +14,29 @@ export default {
   },
 };
 
-const Template = (args) => {
-  return (
-    <Select.Root {...args} defaultValue="ecuador" open={true}>
-      <Select.Trigger aria-label="Countries">
-        <Select.Label>Countries</Select.Label>
-        <Select.Value />
-      </Select.Trigger>
-      <Select.Content>
-        <Select.Group label="Europe">
-          <Select.Item value="france">France</Select.Item>
-          <Select.Item value="united-kingdom">
-            United Kingdom - Scotland, Ireland, Wales, Great Britain, and the
-            commonwealth states of Canada, Australia, Turks and Caicos
-          </Select.Item>
-          <Select.Item value="spain">Spain</Select.Item>
-        </Select.Group>
-        <Select.Group label="South America">
-          <Select.Item value="peru">Peru</Select.Item>
-          <Select.Item value="chile">Chile</Select.Item>
-          <Select.Item value="ecuador">Ecuador</Select.Item>
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
-  );
-};
+const Template = (args) => (
+  <Select.Root {...args} defaultValue="ecuador">
+    <Select.Trigger aria-label="Countries">
+      <Select.Label>Countries</Select.Label>
+      <Select.Value />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Group label="Europe">
+        <Select.Item value="france">France</Select.Item>
+        <Select.Item value="united-kingdom">
+          United Kingdom - Scotland, Ireland, Wales, Great Britain, and the
+          commonwealth states of Canada, Australia, Turks and Caicos
+        </Select.Item>
+        <Select.Item value="spain">Spain</Select.Item>
+      </Select.Group>
+      <Select.Group label="South America">
+        <Select.Item value="peru">Peru</Select.Item>
+        <Select.Item value="chile">Chile</Select.Item>
+        <Select.Item value="ecuador">Ecuador</Select.Item>
+      </Select.Group>
+    </Select.Content>
+  </Select.Root>
+);
 
 export const Play = Template.bind({});
 
@@ -52,3 +50,32 @@ Play.args = {
 };
 
 Play.storyName = "Select";
+
+const InteractionsTemplate = () => (
+  <Select.Root>
+    <Select.Trigger aria-label="Countries">
+      <Select.Label>Countries</Select.Label>
+      <Select.Value />
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Group label="Europe">
+        <Select.Item value="france">France</Select.Item>
+        <Select.Item value="united-kingdom">
+          United Kingdom - Scotland, Ireland, Wales, Great Britain, and the
+          commonwealth states of Canada, Australia, Turks and Caicos
+        </Select.Item>
+        <Select.Item value="spain">Spain</Select.Item>
+      </Select.Group>
+      <Select.Group label="South America">
+        <Select.Item value="peru">Peru</Select.Item>
+        <Select.Item value="chile">Chile</Select.Item>
+        <Select.Item value="ecuador">Ecuador</Select.Item>
+      </Select.Group>
+    </Select.Content>
+  </Select.Root>
+);
+
+export const Interactions = InteractionsTemplate.bind({});
+Interactions.parameters = {
+  chromatic: { disableSnapshot: true },
+};
