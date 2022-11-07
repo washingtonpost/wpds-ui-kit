1ef723d9b79d3f01da2378b4e3a514d53c711819
Claire Helms <claire.helms@washpost.com>
2022-11-07 10:54:22 -0500

    feat(ui-kit): add card component and stories

---
+++ ui/card/src/Card.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import type * as WPDS from "@washingtonpost/wpds-theme";
+import { theme, styled } from "@washingtonpost/wpds-theme";
+
+const NAME = "Card";
+
+interface CardProps extends React.ComponentPropsWithRef<"div"> {
+  /**
+   * The input's label text, required for accessibility
+   */
+  label: string;
+  css?: WPDS.CSS;
+}
+
+const StyledCard = styled("div", {
+  padding: theme.space["100"],
+  // border: theme.colors.subtle,
+  // borderWidth: "1px",
+  // borderStyle: "solid",
+  boxShadow: theme.shadows["200"], //Shadow example
+  backgroundColor: theme.colors.secondary,
+  color: theme.colors.onSecondary,
+});
+
+// styled components here
+
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  (props, ref) => {
+    return (
+      <StyledCard ref={ref} css={props.css} aria-label={props.label}>
+        {props.children}
+      </StyledCard>
+    );
+  }
+);
+
+Card.displayName = NAME;
+
+export type { CardProps };
---
+++ ui/card/src/play.stories.jsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import { Card as Component } from "./";
+import { theme, styled } from "@washingtonpost/wpds-theme";
+import { Button } from "@washingtonpost/wpds-button";
+import { Accordion, ACCORDION_TYPE } from "@washingtonpost/wpds-accordion";
+import { AccordionTrigger } from "@radix-ui/react-accordion";
+
+export default {
+  title: "Card",
+  component: Component,
+};
+
+const Stack = styled("section", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "$100",
+  marginBlockStart: "$200",
+  borderRadius: "$075",
+});
+
+const Label = styled("h3", {
+  color: theme.colors.primary,
+  margin: 0,
+  textAlign: "center",
+});
+
+const DefaultTemplate = (args) => (
+  <Stack>
+    <Label>Card</Label>
+    <Component {...args} css={{ display: "flex" }}>
+      <div>
+        <h4>Hello there! I am a card</h4>
+        <p>
+          I can support many different nested components, check out my other
+          stories to see for yourself!
+        </p>
+      </div>
+      <img
+        height="140"
+        width="auto"
+        layout="fixed"
+        alt="Lava pooling from a volcano"
+        src="https://images.pexels.com/photos/235807/pexels-photo-235807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      />
+    </Component>
+  </Stack>
+);
+
+export const Card = DefaultTemplate.bind({});
+Card.args = { label: "Card component" };
+
+const OptionsT = (args) => (
+  <Stack>
+    <Label>Card with button</Label>
+    <Component css={{ display: "flex", alignItems: "center", gap: "$075" }}>
+      <p>Brand new digital content free with subscription!</p>
+      <Button variant="primary">Subscribe</Button>
+    </Component>
+
+    <Label>Card with image and link</Label>
+    <Component css={{ display: "flex", alignItems: "center", gap: "$100" }}>
+      <img
+        height="140"
+        width="auto"
+        layout="fixed"
+        alt="Vibrant quinoa bowl with carrots, tomatoes, and cucumbers"
+        src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg"
+      />
+      <a href="">
+        How Saria makes ready-to-eat quinoa bowls with lots of veggies.
+      </a>
+    </Component>
+
+    <Label>Card with accordion</Label>
+    <Component
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "0",
+      }}
+    >
+      <h4>Weeknight Ramen Bowls</h4>
+      <Accordion.Root type={ACCORDION_TYPE.single} collapsible={true}>
+        <Accordion.Item value={"item-1"}>
+          <Accordion.Trigger css={{ padding: "$100" }}>
+            View the recipe
+          </Accordion.Trigger>
+          <Accordion.Content css={{ padding: "$100" }}>
+            Step 1 Add one cup of water to a medium pot and bring to a boil. Add
+            the coconut milk, noodles, green beans and peanut butter, if using,
+            and cook, stirring now and then, for about 3 minutes. Step 2 Remove
+            from the heat and add the curry powder and half of the spice mix
+            from the ramen packet and stir until fully dissolved, about 1
+            minute. (Discard the remaining spice mix.) Transfer the ramen and
+            its broth to a large bowl and place the cheese on top. Dust with
+            more curry powder and serve.
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </Component>
+  </Stack>
+);
+export const Options = OptionsT.bind({});
+Options.args = { label: "Card component" };
