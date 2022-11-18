4a660d56ca2286c9f8800a40bd51313b5926bd52
Cullen Rombach <cullen.rombach@washpost.com>
2022-11-18 14:09:36 -0500

    this might work, but I don't know if it's a good idea

--- ui/icon/src/icon.tsx
+++ ui/icon/src/icon.tsx
@@ -12,7 +12,7 @@ export type WPDSThemeColorObject = {
   prefix: string;
 };
 
-interface IconInterface extends React.SVGProps<HTMLOrSVGElement> {
+interface IconInterface extends Omit<React.SVGProps<HTMLOrSVGElement>, 'fill'> {
   /**
    * The name of the icon to display.
    */
