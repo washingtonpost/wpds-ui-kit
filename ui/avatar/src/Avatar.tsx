c01043f28ec907789ad664598662fc5b77bddc77
Andrew Berg <andrew.berg@washpost.com>
2022-12-01 13:27:54 -0500

    feat: refactor Avatar to better handle style and size variations

--- ui/avatar/src/Avatar.tsx
+++ ui/avatar/src/Avatar.tsx
@@ -1,23 +1,13 @@
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { css, styled } from "@washingtonpost/wpds-theme";
+import * as Tokens from "@washingtonpost/wpds-theme/src/tokens";
+
 import type * as WPDS from "@washingtonpost/wpds-theme";
-import * as Theme from "@washingtonpost/wpds-theme";
-import { theme, styled } from "@washingtonpost/wpds-theme";
+import type * as Stitches from "@stitches/react";
 
 const NAME = "Avatar";
 const DEFAULT_AVATAR_SIZE = "200";
-
-interface AvatarProps {
-  /**
-   * Sizes - supports any size token
-   * @default 200
-   *
-   * */
-  size?: "200" | number;
-  /** Override CSS */
-  css?: WPDS.CSS;
-  children: React.ReactElement;
-}
 
 const StyledAvatar = styled(AvatarPrimitive.Root, {
   display: "inline-flex",
@@ -28,36 +18,124 @@ const StyledAvatar = styled(AvatarPrimitive.Root, {
   userSelect: "none",
   borderRadius: "100%",
   length: 0,
+  variants: {
+    size: {
+      "025": {
+        width: "0.25rem",
+        height: "0.25rem",
+      },
+      "050": {
+        width: "0.5rem",
+        height: "0.5rem",
+      },
+      "075": {
+        width: "0.75rem",
+        height: "0.75rem",
+      },
+      "087": {
+        width: "0.875rem",
+        height: "0.875rem",
+      },
+      100: {
+        width: "1rem",
+        height: "1rem",
+      },
+      125: {
+        width: "1.25rem",
+        height: "1.25rem",
+      },
+      150: {
+        width: "1.5rem",
+        height: "1.5rem",
+      },
+      175: {
+        width: "1.75rem",
+        height: "1.75rem",
+      },
+      200: {
+        width: "2rem",
+        height: "2rem",
+      },
+      225: {
+        width: "2.25rem",
+        height: "2.25rem",
+      },
+      250: {
+        width: "2.5rem",
+        height: "2.5rem",
+      },
+      275: {
+        width: "2.75rem",
+        height: "2.75rem",
+      },
+      300: {
+        width: "3rem",
+        height: "3rem",
+      },
+      350: {
+        width: "3.5rem",
+        height: "3.5rem",
+      },
+      400: {
+        width: "4rem",
+        height: "4rem",
+      },
+      450: {
+        width: "4.5rem",
+        height: "4.5rem",
+      },
+      500: {
+        width: "5rem",
+        height: "5rem",
+      },
+    },
+  },
 });
 
-export const Avatar: React.FC<AvatarProps> = React.forwardRef<
-  HTMLElement,
-  AvatarProps
->(({ css, children, size = DEFAULT_AVATAR_SIZE }, ref) => {
-  const child = React.Children.only(children);
+type SizeToken = {
+  prefix: "wpds";
+  scale: "sizes";
+  token: keyof typeof Tokens.sizes;
+  value: string;
+};
 
-  const ImageStyles = Theme.css({
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-    borderRadius: "inherit",
-    length: 0,
-  });
+type StitchesSize = Stitches.VariantProps<typeof StyledAvatar>["size"];
 
-  return (
-    <StyledAvatar
-      ref={ref}
-      css={{
-        width: theme.sizes[size],
-        height: theme.sizes[size],
-        ...css,
-      }}
-    >
-      {React.cloneElement(child, {
-        className: `${ImageStyles}`,
-      })}
-    </StyledAvatar>
-  );
-});
+type AvatarProps = {
+  /** Override CSS */
+  css?: WPDS.CSS;
+  children: React.ReactElement;
+  /**
+   * Sizes - supports any size token
+   * @default 200
+   * */
+  size: StitchesSize | SizeToken;
+} & Omit<React.ComponentPropsWithRef<typeof StyledAvatar>, "size">;
+
+export const Avatar = React.forwardRef<HTMLElement, AvatarProps>(
+  ({ children, size = DEFAULT_AVATAR_SIZE, ...props }, ref) => {
+    const child = React.Children.only(children);
+    const ImageStyles = css({
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+      borderRadius: "inherit",
+      length: 0,
+    });
+
+    let _size = size;
+    if (size instanceof Object && size.token) {
+      _size = size.token;
+    }
+
+    return (
+      <StyledAvatar ref={ref} size={_size as StitchesSize} {...props}>
+        {React.cloneElement(child, {
+          className: `${ImageStyles}`,
+        })}
+      </StyledAvatar>
+    );
+  }
+);
 
 Avatar.displayName = NAME;
