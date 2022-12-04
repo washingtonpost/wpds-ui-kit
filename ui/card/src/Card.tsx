778cb057489dd911483de216d7a909644cd8a24d
Claire Helms <claire.helms@washpost.com>
2022-12-04 11:13:03 -0500

    fixed build

--- ui/card/src/Card.tsx
+++ ui/card/src/Card.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import type * as WPDS from "@washingtonpost/wpds-theme";
 import { theme, styled } from "@washingtonpost/wpds-theme";
 
-const NAME = "Card";
-type CardType = { CSS?: WPDS.CSS } & HTMLDivElement;
-
 const StyledCard = styled("div", {
   padding: theme.space["100"],
   border: theme.colors.subtle,
@@ -14,14 +11,15 @@ const StyledCard = styled("div", {
   color: theme.colors.onSecondary,
 });
 
-export const Card = React.forwardRef<CardType>(
-  (props, ref) => {
-    return (
-      <StyledCard ref={ref} {...props}>
-        {props.children}
-      </StyledCard>
-    );
+type CardProps = {
+  CSS?: WPDS.CSS;
+  children?: React.ReactNode;
+} & React.ComponentPropsWithRef<typeof StyledCard>;
+
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ children, css, ...props }: CardProps, ref) => {
+    return <StyledCard css={css} {...props}>{children}</StyledCard>;
   }
 );
 
-Card.displayName = NAME;
+Card.displayName = "Card";
