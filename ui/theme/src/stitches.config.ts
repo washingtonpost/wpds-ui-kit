a51d78e8ef768e5a0cc458697e5b00e35a33606a
atomictangerine <mbeguiluz@gmail.com>
2022-12-12 14:05:19 -0500

    fix: update const name

--- ui/theme/src/stitches.config.ts
+++ ui/theme/src/stitches.config.ts
@@ -182,7 +182,7 @@ export const darkModeGlobalStyles = globalCss({
   },
 });
 
-export const globalAnimations = globalCss({
+export const globalJsTriggerAnimations = globalCss({
   "@keyframes jsTriggerAutoFillStart": {
     from: {
       alpha: 1,
