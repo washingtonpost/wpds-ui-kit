362629d0b5ae7b4dd68111f5d095f53848c96976
wp-aberg <102534985+wp-aberg@users.noreply.github.com>
2023-01-13 17:30:27 -0500

    feat: add Popover component (#301)

---
+++ ui/popover/src/PopoverPortal.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+
+import type { PopoverPortalProps as RadixPopoverPortalProps } from "@radix-ui/react-popover";
+
+const NAME = "PopoverPortal";
+
+export type PopoverPortalProps = RadixPopoverPortalProps;
+
+export const PopoverPortal = (props: PopoverPortalProps) => (
+  <PopoverPrimitive.Portal {...props}>{props.children}</PopoverPrimitive.Portal>
+);
+
+PopoverPortal.displayName = NAME;
