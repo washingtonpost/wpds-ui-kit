9aa64237d8e42563839ff1dbde959ea8cefbe548
atomictangerine <mbeguiluz@gmail.com>
2023-03-09 13:02:46 -0500

    chore: fix build error

--- ui/tabs/src/TabsTrigger.tsx
+++ ui/tabs/src/TabsTrigger.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useEffect } from "react";
-import { CSSTransition } from "react-transition-group";
+// import { CSSTransition } from "react-transition-group";
 
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 
