2c83b4f28cc0ef93327046e8d5a993ca49b78e59
Andrew Berg <andrew.berg@washpost.com>
2023-06-09 10:17:48 -0400

    feat: use wpds css vars for tailwind theme

--- ui/tailwind-theme/script/create-theme.js
+++ ui/tailwind-theme/script/create-theme.js
@@ -8,7 +8,6 @@ const tokens = require("@washingtonpost/wpds-theme/src/wpds.tokens.json");
 let theme = {
   borderRadius: {
     none: "0px",
-    full: "9999px",
   },
   boxShadow: {
     none: "none",
@@ -21,8 +20,8 @@ let theme = {
     white: tokens.color.static.gray700.value,
   },
   fontFamily: {
-    sans: tokens.fonts.meta.value.split(","),
-    serif: tokens.fonts.meta.value.split(","),
+    sans: "var(--wpds-fonts-meta)",
+    serif: "var(--wpds-fonts-body)",
   },
   spacing: {
     px: "1px",
@@ -32,106 +31,6 @@ let theme = {
     auto: "auto",
   },
 };
-
-function convertTokens() {
-  for (var key in tokens) {
-    switch (key) {
-      case "color":
-        handleColors();
-        break;
-      case "fonts":
-        handleFonts();
-        break;
-      case "fontSize":
-        handleConversion({ tokenKey: "fontSize", themeKey: "fontSize" });
-        break;
-      case "fontWeights":
-        handleConversion({ tokenKey: "fontWeights", themeKey: "fontWeight" });
-        break;
-      case "lineHeight":
-        handleConversion({ tokenKey: "lineHeight", themeKey: "lineHeight" });
-        break;
-      case "radii":
-        handleConversion({ tokenKey: "radii", themeKey: "borderRadius" });
-        break;
-      case "shadow":
-        handleConversion({ tokenKey: "shadow", themeKey: "boxShadow" });
-        break;
-      case "size":
-        handleConversion({ tokenKey: "size", themeKey: "spacing" });
-        break;
-      case "z-index":
-        handleConversion({ tokenKey: "z-index", themeKey: "zIndex" });
-        break;
-    }
-  }
-}
-
-function handleColors() {
-  Object.keys(tokens.color).forEach((key) => {
-    if (key === "static") return;
-    Object.keys(tokens.color[key]).forEach((color) => {
-      if (color === "description") return;
-
-      if (key === "theme") {
-        let valueColor = tokens.color[key][color].value.substring(
-          1,
-          tokens.color[key][color].value.length - 1
-        );
-
-        if (valueColor.includes("-static")) {
-          valueColor = valueColor.substring(0, valueColor.indexOf("-"));
-          theme.colors[color] = tokens.color.static[valueColor].value;
-        } else {
-          theme.colors[color] = tokens.color.light[valueColor].value;
-          theme.colors[color + "-dark"] = tokens.color.dark[valueColor].value;
-        }
-      } else {
-        const numIndex = color
-          .split("")
-          .findIndex((char) => !isNaN(parseInt(char, 10)));
-        const colorName = color.substring(0, numIndex);
-        let colorNumber = color.substring(numIndex);
-        if (key === "dark") {
-          colorNumber += "-dark";
-        }
-        if (theme.colors[colorName] === undefined) {
-          theme.colors[colorName] = {};
-        }
-        theme.colors[colorName][colorNumber] = tokens.color[key][color].value;
-      }
-    });
-  });
-}
-
-function handleFonts() {
-  Object.keys(tokens.fonts).forEach((key) => {
-    theme.fontFamily[key] = tokens.fonts[key].value.split(",");
-  });
-}
-
-function handleConversion({ tokenKey, themeKey }) {
-  Object.keys(tokens[tokenKey]).forEach((key) => {
-    if (key === "description") return;
-
-    if (theme[themeKey] === undefined) {
-      theme[themeKey] = {};
-    }
-
-    if (
-      typeof tokens[tokenKey][key].value === "string" &&
-      tokens[tokenKey][key].value.includes("{")
-    ) {
-      const keys = tokens[tokenKey][key].value
-        .substring(1, tokens[tokenKey][key].value.length - 1)
-        .split(".");
-      const value = tokens[keys[0]][keys[1]].value;
-      theme[themeKey][key] = value;
-    } else {
-      theme[themeKey][key] = tokens[tokenKey][key].value;
-    }
-  });
-}
 
 function createThemeFile() {
   try {
@@ -154,3 +53,97 @@ function createThemeFile() {
 }
 
 createThemeFile();
+
+function convertTokens() {
+  for (var key in tokens) {
+    let themeKey;
+
+    switch (key) {
+      case "color":
+        themeKey = "colors";
+        break;
+      case "fonts":
+        themeKey = "fontFamily";
+        break;
+      case "fontSize":
+        themeKey = "fontSize";
+        break;
+      case "fontWeights":
+        themeKey = "fontWeight";
+        break;
+      case "lineHeight":
+        themeKey = "lineHeight";
+        break;
+      case "radii":
+        themeKey = "borderRadius";
+        break;
+      case "shadow":
+        themeKey = "boxShadow";
+        break;
+      case "size":
+        themeKey = "spacing";
+        break;
+      case "z-index":
+        themeKey = "zIndex";
+        break;
+    }
+    if (themeKey === "colors") {
+      handleColors();
+    } else {
+      handleConversion({ tokenKey: key, themeKey: themeKey });
+    }
+  }
+}
+
+function handleColors() {
+  Object.keys(tokens.color).forEach((key) => {
+    Object.keys(tokens.color[key]).forEach((color) => {
+      if (color === "description") return;
+      if (key === "dark") return;
+
+      const colorVar = `var(--wpds-colors-${color}${
+        key === "static" ? `-${key}` : ""
+      })`;
+
+      if (key === "theme") {
+        theme.colors[color] = colorVar;
+      } else {
+        const numIndex = color
+          .split("")
+          .findIndex((char) => !isNaN(parseInt(char, 10)));
+        const colorName = color.substring(0, numIndex);
+        let colorNumber =
+          color.substring(numIndex) + (key === "static" ? `-${key}` : "");
+        if (theme.colors[colorName] === undefined) {
+          theme.colors[colorName] = {};
+        }
+        theme.colors[colorName][colorNumber] = colorVar;
+      }
+    });
+  });
+}
+
+function handleConversion({ tokenKey, themeKey }) {
+  Object.keys(tokens[tokenKey]).forEach((key) => {
+    if (key === "description") return;
+
+    if (theme[themeKey] === undefined) {
+      theme[themeKey] = {};
+    }
+    theme[themeKey][key] = `var(--wpds-${getVarFromKey(tokenKey)}-${key})`;
+  });
+}
+
+function getVarFromKey(key) {
+  switch (key) {
+    case "fontSize":
+    case "size":
+    case "shadow":
+    case "lineHeight":
+      return key + "s";
+    case "z-index":
+      return "zIndices";
+    default:
+      return key;
+  }
+}
