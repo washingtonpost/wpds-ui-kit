b228eaf001c37e970540ee7540210a41b123b871
Edward Granger <edward.granger@washpost.com>
2023-06-13 17:08:50 -0400

    wip

--- ui/dropdown-menu/src/DropdownMenu.tsx
+++ ui/dropdown-menu/src/DropdownMenu.tsx
@@ -4,38 +4,40 @@ import * as React from "react";
 // import { ChevronRight } from "@washingtonpost/wpds-assets";
 // import { Check } from "@washingtonpost/wpds-assets";
 // import { StarFull } from "@washingtonpost/wpds-assets";
-import { styled, theme, CSS } from "@washingtonpost/wpds-theme";
+import { CSS } from "@washingtonpost/wpds-theme";
 import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
 
 // const NAME = "DropdownMenu";
 
-const StyledDropdownMenu = styled("div", {
-  appearance: "none",
-  background: "transparent",
-  border: "none",
-  color: theme.colors.primary,
-  cursor: "pointer",
-  display: "inline-flex",
-  alignItems: "center",
-  gap: theme.space["025"],
-  fontFamily: theme.fonts.meta,
-  fontSize: theme.fontSizes["100"],
-  lineHeight: theme.lineHeights.meta,
-  padding: theme.space["050"],
-});
+// const StyledDropdownMenu = styled("div", {
+//   appearance: "none",
+//   background: "transparent",
+//   border: "none",
+//   color: theme.colors.primary,
+//   cursor: "pointer",
+//   display: "inline-flex",
+//   alignItems: "center",
+//   gap: theme.space["025"],
+//   fontFamily: theme.fonts.meta,
+//   fontSize: theme.fontSizes["100"],
+//   lineHeight: theme.lineHeights.meta,
+//   padding: theme.space["050"],
+// });
 
 export type DropdownMenuProps = {
   /** Any React node may be used as a child to allow for formatting */
   children?: React.ReactNode;
   /** Override CSS */
   css?: CSS;
-} & React.ComponentPropsWithRef<typeof StyledDropdownMenu>;
+} & React.ComponentPropsWithRef<typeof RadixDropdownMenu.Root>;
 
 export const DropdownMenu = React.forwardRef<HTMLDivElement, DropdownMenuProps>
   (({ children, ...props }, ref) => {
     return (
-      <StyledDropdownMenu {...props} ref={ref}>
+      <RadixDropdownMenu.Root {...props}>
         {children}
-      </StyledDropdownMenu>
+      </RadixDropdownMenu.Root>
     );
   });
+
+export const _RadixDropdownMenu = RadixDropdownMenu; 
--- ui/dropdown-menu/src/play.stories.tsx
+++ ui/dropdown-menu/src/play.stories.tsx
@@ -1,27 +1,123 @@
-import { DropdownMenu as Component } from "./DropdownMenu";
+import { DropdownMenu as ComponentRoot, _RadixDropdownMenu as Component } from "./DropdownMenu";
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
     title: "DropdownMenu",
-    component: Component,
-} as ComponentMeta<typeof Component>
+    component: ComponentRoot,
+} as ComponentMeta<typeof ComponentRoot>
 
-
-const Template: ComponentStory<typeof Component> = (
+const Template: ComponentStory<typeof ComponentRoot> = (
     { children, ...args },
     context
 ) => {
+
+    // return (
+    //     <>
+    //         <Component {...args} data-testid={`${context.theme}-skip-link`}>
+    //             {children}
+    //         </Component>
+    //     </>
+    // );
+
+    // const [bookmarksChecked, setBookmarksChecked] = React.useState(true);
+    // const [urlsChecked, setUrlsChecked] = React.useState(false);
+    // const [person, setPerson] = React.useState('pedro');
+
     return (
-        <>
-            <Component {...args} data-testid={`${context.theme}-skip-link`}>
-                {children}
-            </Component>
-        </>
+        <ComponentRoot>
+            <Component.Trigger asChild>
+                <button className="IconButton" aria-label="Customise options">
+                    {/* <Menu /> */}
+                </button>
+            </Component.Trigger>
+
+            <Component.Portal>
+                <Component.Content className="DropdownMenuContent" sideOffset={5}>
+                    <Component.Item className="DropdownMenuItem">
+                        New Tab <div className="RightSlot">⌘+T</div>
+                    </Component.Item>
+                    <Component.Item className="DropdownMenuItem">
+                        New Window <div className="RightSlot">⌘+N</div>
+                    </Component.Item>
+                    <Component.Item className="DropdownMenuItem" disabled>
+                        New Private Window <div className="RightSlot">⇧+⌘+N</div>
+                    </Component.Item>
+                    <Component.Sub>
+                        <Component.SubTrigger className="DropdownMenuSubTrigger">
+                            More Tools
+                            <div className="RightSlot">
+                                <div />
+                            </div>
+                        </Component.SubTrigger>
+                        <Component.Portal>
+                            <Component.SubContent
+                                className="DropdownMenuSubContent"
+                                sideOffset={2}
+                                alignOffset={-5}
+                            >
+                                <Component.Item className="DropdownMenuItem">
+                                    Save Page As… <div className="RightSlot">⌘+S</div>
+                                </Component.Item>
+                                <Component.Item className="DropdownMenuItem">Create Shortcut…</Component.Item>
+                                <Component.Item className="DropdownMenuItem">Name Window…</Component.Item>
+                                <Component.Separator className="DropdownMenu.Separator" />
+                                <Component.Item className="DropdownMenuItem">Developer Tools</Component.Item>
+                            </Component.SubContent>
+                        </Component.Portal>
+                    </Component.Sub>
+
+                    <Component.Separator className="DropdownMenuSeparator" />
+
+                    <Component.CheckboxItem
+                        className="DropdownMenuCheckboxItem"
+                        checked={true}
+                        onCheckedChange={() => { }}
+                    >
+                        <Component.ItemIndicator className="DropdownMenuItemIndicator">
+                            <div />
+                        </Component.ItemIndicator>
+                        Show Bookmarks <div className="RightSlot">⌘+B</div>
+                    </Component.CheckboxItem>
+                    <Component.CheckboxItem
+                        className="DropdownMenuCheckboxItem"
+                        checked={true}
+                        onCheckedChange={() => { }}
+                    >
+                        <Component.ItemIndicator className="DropdownMenuItemIndicator">
+                            <div />
+                        </Component.ItemIndicator>
+                        Show Full URLs
+                    </Component.CheckboxItem>
+
+                    <Component.Separator className="DropdownMenuSeparator" />
+
+                    <Component.Label className="DropdownMenuLabel">People</Component.Label>
+                    <Component.RadioGroup value={""} onValueChange={() => { }}>
+                        <Component.RadioItem className="DropdownMenuRadioItem" value="pedro">
+                            <Component.ItemIndicator className="DropdownMenuItemIndicator">
+                                {/* <StarFull /> */}
+                            </Component.ItemIndicator>
+                            Pedro Duarte
+                        </Component.RadioItem>
+                        <Component.RadioItem className="DropdownMenuRadioItem" value="colm">
+                            <Component.ItemIndicator className="DropdownMenuItemIndicator">
+                                {/* <StarFull /> */}
+                            </Component.ItemIndicator>
+                            Colm Tuite
+                        </Component.RadioItem>
+                    </Component.RadioGroup>
+
+                    <Component.Arrow className="DropdownMenuArrow" />
+                </Component.Content>
+            </Component.Portal>
+        </ComponentRoot>
     );
+
+
 };
 
-export const Button = Template.bind({});
+export const DropdownMenu = Template.bind({});
 
-Button.args = {
+DropdownMenu.args = {
     children: "Text button",
 };
