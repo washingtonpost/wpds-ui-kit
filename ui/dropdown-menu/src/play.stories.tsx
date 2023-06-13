d35989c64bcfa2974df94a0c7377da7db627cb7c
Caroline Dinh <Caroline.Dinh@WashPost.com>
2023-06-13 16:19:15 -0400

    Start integrating dropdownmenu component

---
+++ ui/dropdown-menu/src/play.stories.tsx
@@ -0,0 +1,7 @@
+import { DropdownMenu as Component } from "./DropdownMenu";
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+    title: "DropdownMenu",
+    component: Component,
+} as ComponentMeta<typeof Component>
