012890da693a99d50919dee7ada4708e6bbe7d9e
Edward Granger <edward.granger@washpost.com>
2023-07-03 13:23:43 -0400

    wip

--- ui/action-menu/src/ActionMenuSubContent.tsx
+++ ui/action-menu/src/ActionMenuSubContent.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { theme, styled } from "@washingtonpost/wpds-theme";
+import { styled } from "@washingtonpost/wpds-theme";
 
 import * as ActionMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
