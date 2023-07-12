ff0a40c3611523a227e114c0a93d1046002f25b8
Caroline Dinh <Caroline.Dinh@WashPost.com>
2023-07-12 11:50:54 -0400

    add Postoni Ultra

--- ui/theme/src/stitches.config.ts
+++ ui/theme/src/stitches.config.ts
@@ -171,17 +171,17 @@ export const globalStyles = globalCss({
       src: "url(https://www.washingtonpost.com/wp-stat/assets/fonts/ITC_Franklin-Light.woff2)",
     },
     {
-      fontFamily: "PostoniUltra",
-      fontWeight: 700,
+      fontFamily: "PostoniDisplayMag",
+      fontWeight: 800,
       fontDisplay: "fallback",
       src: "url(https://www.washingtonpost.com/wp-stat/assets/fonts/PostoniDisplayMag-Ultra.woff2)",
     },
     {
-      fontFamily: "PostoniUltra",
-      fontWeight: 700,
+      fontFamily: "PostoniDisplayMag",
+      fontWeight: 800,
       fontDisplay: "fallback",
       fontStyle: "italic",
-      src: "url(https://www.washingtonpost.com/wp-stat/assets/fonts/PostoniDisplayMag-UltraItalic.woff2)",
+      src: "url(https://www.washingtonpost.com/wp-stat/assets/fonts/PostoniDisplayMag-Ultra_Italic.woff2)",
     },
   ],
 });
