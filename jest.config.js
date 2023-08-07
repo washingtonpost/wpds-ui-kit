1efc3160b758201c8f59fd4d9f830e89af85150a
Edward Granger <edward.granger@washpost.com>
2023-08-07 15:53:38 -0400

    wip

---
+++ jest.config.js
@@ -0,0 +1,46 @@
+
+/** @type {import("jest").Config} */
+module.exports = {
+  roots: [
+    "./ui"
+  ],
+  preset: "ts-jest/presets/js-with-babel-legacy",
+  testEnvironment: "./custom-env.ts",
+  cacheDirectory: ".jest-cache",
+  collectCoverage: true,
+  coverageDirectory: "jest-coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    ".stories.js",
+    "index.js"
+  ],
+  coverageProvider: "v8",
+  coverageReporters: [
+    "html",
+    "text"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 65,
+      lines: 80
+    }
+  },
+  setupFilesAfterEnv: [
+    "./scripts/setupTests.ts"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/eslint-plugin/"
+  ],
+  // transform: {
+  //   '^.+\\.(js|jsx|ts|tsx|mjs)$': 'ts-jest',
+  // },
+  transformIgnorePatterns: [`/node_modules/(?!(nanoid))`],
+  testMatch: [
+    "**/*.test.[jt]s?(x)"
+  ],
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1"
+  }
+};
