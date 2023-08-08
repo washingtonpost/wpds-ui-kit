4f604bd9aeed6bac3895175a61874cc002af1462
Edward Granger <edward.granger@washpost.com>
2023-08-08 16:22:37 -0400

    chore: clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,3 @@
-/*
- * Dependencies that only export ES modules
- *
- * ESM isn't supported by jest-environment-jsdom so these dependencies
- * need to be transformed into CommonJS, like our source code, to play nicely
- * with jest
- */
-const esModules = [
-  "@accessible/accordion",
-  "@accessible/button",
-  "@accessible/tabbable",
-  "@accessible/use-conditional-focus",
-  "@accessible/use-id",
-  "@accessible/use-keycode",
-  "@react-hook/event",
-  "@react-hook/latest",
-  "@react-hook/merged-ref",
-  "@react-hook/throttle",
-  "@react-hook/passive-layout-effect",
-  "@react-hook/window-scroll",
-  "nanoid",
-  "uuid"
-];
-
 /** @type {import("jest").Config} */
 module.exports = {
   "roots": [
