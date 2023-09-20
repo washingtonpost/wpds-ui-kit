c37d2cf5d70a9b11247476390c87479b70b79998
Edward Granger <edward.granger@washpost.com>
2023-09-20 12:17:27 -0400

    chore: add cities db file

---
+++ ui/input-search/src/cities-db.ts
@@ -0,0 +1,222 @@
+export const citiesDB = [
+  {
+    "uuid": "1a3b8f79-9a2d-4f5c-9a22-21c34f6e345a",
+    "city": "New York",
+    "state": "New York"
+  },
+  {
+    "uuid": "a2f5c4d2-8a9d-1b3f-4a5e-6c7b2f3a1d0e",
+    "city": "Los Angeles",
+    "state": "California"
+  },
+  {
+    "uuid": "c7b1e2d3-4f8e-5a6d-2c9d-1b3a0f8c7b9a",
+    "city": "Chicago",
+    "state": "Illinois"
+  },
+  {
+    "uuid": "4a5e2d3b-9a8d-1b3f-4c5e-6c7b2f3a1d0e",
+    "city": "Houston",
+    "state": "Texas"
+  },
+  {
+    "uuid": "b3a0f8c7-7b1e-2d3f-4f8e-5a6d2c9d1b3a",
+    "city": "Phoenix",
+    "state": "Arizona"
+  },
+  {
+    "uuid": "8c7b1e2d-34f6-4e5a-9a2d-1b3f4c5e6c7b",
+    "city": "Philadelphia",
+    "state": "Pennsylvania"
+  },
+  {
+    "uuid": "2c9d1b3a-0f8c-7b1e-2d3f-4f8e5a6d2c9d",
+    "city": "San Antonio",
+    "state": "Texas"
+  },
+  {
+    "uuid": "9a8d1b3f-4c5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "San Diego",
+    "state": "California"
+  },
+  {
+    "uuid": "5a6d2c9d-1b3a-0f8c-7b1e-2d3f4f8e5a6d",
+    "city": "Dallas",
+    "state": "Texas"
+  },
+  {
+    "uuid": "6c7b2f3a-1d0e-4a5e-2d3b-9a8d1b3f4c5e",
+    "city": "San Jose",
+    "state": "California"
+  },
+  {
+    "uuid": "1d0e4a5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "Austin",
+    "state": "Texas"
+  },
+  {
+    "uuid": "3f4c5e6c-7b2f-3a1d-0e4a-5e2d3b9a8d1b",
+    "city": "Jacksonville",
+    "state": "Florida"
+  },
+  {
+    "uuid": "8e5a6d2c-9d1b-3a0f-8c7b-1e2d3f4f8e5a",
+    "city": "San Francisco",
+    "state": "California"
+  },
+  {
+    "uuid": "2d3f4f8e-5a6d-2c9d-1b3a-0f8c7b1e2d3f",
+    "city": "Indianapolis",
+    "state": "Indiana"
+  },
+  {
+    "uuid": "4f8e5a6d-2c9d-1b3a-0f8c-7b1e2d3f4f8e",
+    "city": "Columbus",
+    "state": "Ohio"
+  },
+  {
+    "uuid": "1b3a0f8c-7b1e-2d3f-4f8e-5a6d2c9d1b3a",
+    "city": "Fort Worth",
+    "state": "Texas"
+  },
+  {
+    "uuid": "6c7b2f3a-1d0e-4a5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "Charlotte",
+    "state": "North Carolina"
+  },
+  {
+    "uuid": "3a1d0e4a-5e2d-3b9a-8d1b-3f4c5e6c7b2f",
+    "city": "Seattle",
+    "state": "Washington"
+  },
+  {
+    "uuid": "5e2d3b9a-8d1b-3f4c-5e6c-7b2f3a1d0e4a",
+    "city": "Denver",
+    "state": "Colorado"
+  },
+  {
+    "uuid": "9a8d1b3f-4c5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "El Paso",
+    "state": "Texas"
+  },
+  {
+    "uuid": "4c5e2d3b-9a8d-1b3f-4c5e-6c7b2f3a1d0e",
+    "city": "Detroit",
+    "state": "Michigan"
+  },
+  {
+    "uuid": "b9a8d1b3-f4c5-6c7b-2f3a-1d0e4a5e2d3b",
+    "city": "Washington D.C.",
+    "state": "District of Columbia"
+  },
+  {
+    "uuid": "4a5e2d3b-9a8d-1b3f-4c5e-6c7b2f3a1d0e",
+    "city": "Boston",
+    "state": "Massachusetts"
+  },
+  {
+    "uuid": "3f4c5e6c-7b2f-3a1d-0e4a-5e2d3b9a8d1b",
+    "city": "Memphis",
+    "state": "Tennessee"
+  },
+  {
+    "uuid": "2d3f4f8e-5a6d-2c9d-1b3a-0f8c7b1e2d3f",
+    "city": "Nashville",
+    "state": "Tennessee"
+  },
+  {
+    "uuid": "1b3a0f8c-7b1e-2d3f-4f8e-5a6d2c9d1b3a",
+    "city": "Portland",
+    "state": "Oregon"
+  },
+  {
+    "uuid": "6c7b2f3a-1d0e-4a5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "Oklahoma City",
+    "state": "Oklahoma"
+  },
+  {
+    "uuid": "3a1d0e4a-5e2d-3b9a-8d1b-3f4c5e6c7b2f",
+    "city": "Las Vegas",
+    "state": "Nevada"
+  },
+  {
+    "uuid": "5e2d3b9a-8d1b-3f4c-5e6c-7b2f3a1d0e4a",
+    "city": "Baltimore",
+    "state": "Maryland"
+  },
+  {
+    "uuid": "9a8d1b3f-4c5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "Louisville",
+    "state": "Kentucky"
+  },
+  {
+    "uuid": "4c5e2d3b-9a8d-1b3f-4c5e-6c7b2f3a1d0e",
+    "city": "Milwaukee",
+    "state": "Wisconsin"
+  },
+  {
+    "uuid": "b9a8d1b3-f4c5-6c7b-2f3a-1d0e4a5e2d3b",
+    "city": "Albuquerque",
+    "state": "New Mexico"
+  },
+  {
+    "uuid": "4a5e2d3b-9a8d-1b3f-4c5e-6c7b2f3a1d0e",
+    "city": "Tucson",
+    "state": "Arizona"
+  },
+  {
+    "uuid": "3f4c5e6c-7b2f-3a1d-0e4a-5e2d3b9a8d1b",
+    "city": "Fresno",
+    "state": "California"
+  },
+  {
+    "uuid": "2d3f4f8e-5a6d-2c9d-1b3a-0f8c7b1e2d3f",
+    "city": "Sacramento",
+    "state": "California"
+  },
+  {
+    "uuid": "1b3a0f8c-7b1e-2d3f-4f8e-5a6d2c9d1b3a",
+    "city": "Kansas City",
+    "state": "Missouri"
+  },
+  {
+    "uuid": "6c7b2f3a-1d0e-4a5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "Long Beach",
+    "state": "California"
+  },
+  {
+    "uuid": "3a1d0e4a-5e2d-3b9a-8d1b-3f4c5e6c7b2f",
+    "city": "Mesa",
+    "state": "Arizona"
+  },
+  {
+    "uuid": "5e2d3b9a-8d1b-3f4c-5e6c-7b2f3a1d0e4a",
+    "city": "Virginia Beach",
+    "state": "Virginia"
+  },
+  {
+    "uuid": "9a8d1b3f-4c5e-2d3b-9a8d-1b3f4c5e6c7b",
+    "city": "Atlanta",
+    "state": "Georgia"
+  },
+  {
+    "uuid": "4c5e2d3b-9a8d-1b3f-4c5e-6c7b2f3a1d0e",
+    "city": "Colorado Springs",
+    "state": "Colorado"
+  },
+  {
+    "uuid": "b9a8d1b3-f4c5-6c7b-2f3a-1d0e4a5e2d3b",
+    "city": "Raleigh",
+    "state": "North Carolina"
+  },
+  {
+    "uuid": "7b2f3a1d-0e4a-5e2d-3b9a-8d1b3f4c5e6c",
+    "city": "Omaha",
+    "state": "Nebraska"
+  },
+  {
+    "uuid": "8d1b3f4c-5e6c-7b2f-3a1d-0e4a5e2d3b9a",
+    "city": "Miami",
+    "state": "Florida"
+  }
+];
