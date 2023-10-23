7ffebb05f6f970385f8f22b05bdcb845fdef4462
Arturo Silva <arturo.silva@washpost.com>
2023-10-22 21:00:48 -0400

    chore: upgrade to storybook v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,29 @@
+import { dirname, join } from "path";
 // add process.env
 require("dotenv").config();
 
 module.exports = {
   stories: ["../ui/(**|!(node_modules))/src/*.stories.@(ts|tsx|js|jsx)"],
   exclude: ["node_modules", "dist"],
+
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
+
   previewBody: (body) => `${body}`,
+
   features: {
     storyStoreV7: true,
     interactionsDebugger: true,
   },
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
+
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {}
   },
+
   typescript: {
     check: true,
     reactDocgen: "react-docgen-typescript",
@@ -33,4 +39,12 @@ module.exports = {
       },
     },
   },
+
+  docs: {
+    autodocs: true
+  }
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
