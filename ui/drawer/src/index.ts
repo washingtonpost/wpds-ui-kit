8f7cacb81b496c192bafde676dd4d603d7116cf7
Arturo Silva <arturo.silva@washpost.com>
2024-02-23 13:11:31 -0500

    lol idk if this is wise

--- ui/drawer/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Drawer";
-export * from "./DrawerRoot";
-export * from "./DrawerContent";
-export * from "./DrawerTrigger";
-export * from "./DrawerClose";
-export * from "./DrawerCustomTrigger";
-export * from "./DrawerScrim";
--- ui/popover/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Popover";
-export * from "./PopoverAnchor";
-export * from "./PopoverClose";
-export * from "./PopoverContent";
-export * from "./PopoverPortal";
-export * from "./PopoverRoot";
-export * from "./PopoverTrigger";
