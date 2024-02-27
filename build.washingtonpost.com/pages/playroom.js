7cbecd0cf40158b59b8175a48e435f23355b4c3d
Arturo Silva <arturo.silva@washpost.com>
2024-02-26 23:24:39 -0500

    chore: commit wip

--- build.washingtonpost.com/pages/playroom.js
+++ build.washingtonpost.com/pages/playroom.js
@@ -1,10 +1,18 @@
 import * as React from "react";
 
-export default function Playroom({ title = "Playroom" }) {
+export default function Playroom({ title }) {
   return (
     <div>
       <h1>{title}</h1>
       <p>Let's play!</p>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      title: "Playroom",
+    },
+  };
 }
