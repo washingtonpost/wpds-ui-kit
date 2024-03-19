6337edf442d3c7278fa90c89247d1976ec86ba7f
Arturo Silva <arturo.silva@washpost.com>
2024-03-19 12:23:41 -0400

    chore: commit wip

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,7 +10,6 @@ import React from "react";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
