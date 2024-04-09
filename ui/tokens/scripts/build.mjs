7f666b1097867aaea0276a7c2b101ad298acbf6f
Arturo Silva <arturo.silva@washpost.com>
2024-04-09 14:03:43 -0400

    chore: commit wip

--- ui/tokens/scripts/build.mjs
+++ ui/tokens/scripts/build.mjs
@@ -1,24 +1,25 @@
 import React from "react";
 import { renderToString } from "react-dom/server.js";
-import { getCssText, globalStyles, darkModeGlobalStyles, darkTheme } from "@washingtonpost/wpds-theme";
+import {
+  getCssText,
+  darkModeGlobalStyles,
+  darkTheme,
+} from "@washingtonpost/wpds-theme";
 import fs from "fs";
 
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const css = renderToString(
-    React.createElement("style", {
-        dangerouslySetInnerHTML: { __html: getCssText() }
-    })
+  React.createElement("style", {
+    dangerouslySetInnerHTML: { __html: getCssText() },
+  })
 );
 
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const div = renderToString(
-    React.createElement("div", {
-        className: darkTheme
-    })
+  React.createElement("div", {
+    className: darkTheme,
+  })
 );
-
-globalStyles();
-darkModeGlobalStyles();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const template = `
