83a68419d32b8d5d7ae3645f476d9c63470533f4
Arturo Silva <arturo.silva@washpost.com>
2024-06-04 23:13:00 -0400

    feat: switch to font display swap

--- build.washingtonpost.com/pages/_document.js
+++ build.washingtonpost.com/pages/_document.js
@@ -42,43 +42,6 @@ export default function Document() {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `@font-face {
-								font-family: Postoni;
-								font-weight: 700;
-								font-display: fallback;
-								src: url(https://www.washingtonpost.com/wp-stat/assets/fonts/PostoniWide-Bold.woff2);
-								unicode-range: U+a, U+20-29, U+2c-5b, U+5d, U+5f, U+61-7d, U+a0, U+a9, U+c9,
-								  U+e0-e3, U+e7, U+e9, U+ea, U+ed, U+f3-f5, U+fa, U+2009, U+2013, U+2014,
-								  U+2018, U+2019, U+201c, U+201d, U+2026;
-							  }
-							  @font-face {
-								font-family: Postoni;
-								font-weight: 300;
-								font-display: fallback;
-								src: url(https://www.washingtonpost.com/wp-stat/assets/fonts/PostoniWide-Regular.woff2);
-							  }
-							  @font-face {
-								font-family: Franklin;
-								font-weight: 700;
-								font-display: fallback;
-								unicode-range: U+a, U+20-29, U+2c-5b, U+5d, U+5f, U+61-7d, U+a0, U+a9, U+c9,
-								  U+e0-e3, U+e7, U+e9, U+ea, U+ed, U+f3-f5, U+fa, U+2009, U+2013, U+2014,
-								  U+2018, U+2019, U+201c, U+201d, U+2026;
-								src: url(https://www.washingtonpost.com/wp-stat/assets/fonts/ITC_Franklin-Bold.woff2);
-							  }
-							  @font-face {
-								font-family: Franklin;
-								font-weight: 300;
-								font-display: fallback;
-								src: url(https://www.washingtonpost.com/wp-stat/assets/fonts/ITC_Franklin-Light.woff2);
-								unicode-range: U+a, U+20-29, U+2c-5b, U+5d, U+5f, U+61-7d, U+a0, U+a9, U+c9,
-								  U+e0-e3, U+e7, U+e9, U+ea, U+ed, U+f1, U+f3-f5, U+fa, U+2009, U+2013,
-								  U+2014, U+2018, U+2019, U+201c, U+201d, U+2026;
-							  }`,
-          }}
-        />
       </Head>
       <body>
         <noscript>
