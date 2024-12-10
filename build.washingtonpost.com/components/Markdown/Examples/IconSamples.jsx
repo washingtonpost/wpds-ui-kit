db888d305b42c149bd09dc07376a02e6de2a29a6
Andrew Berg <andrew.berg@washpost.com>
2024-12-10 11:39:04 -0500

    chore: formatting

--- build.washingtonpost.com/components/Markdown/Examples/IconSamples.jsx
+++ build.washingtonpost.com/components/Markdown/Examples/IconSamples.jsx
@@ -18,163 +18,163 @@ import { logoList } from "./LogoSamples";
  */
 
 const SuccessToast = ({ Name }) => {
-	return (
-		<AlertBanner.Root variant="success">
-			<AlertBanner.Content css={{ minWidth: 250, paddingRight: "$050" }}>
-				<b>Copied: </b>
-				<Box as="span" css={{ fontSize: 16 }}>
-					Import statement for{" "}
-					<Box as="i" css={{ textTransform: "capitalize" }}>
-						{Name}
-					</Box>
-				</Box>
-			</AlertBanner.Content>
-		</AlertBanner.Root>
-	);
+  return (
+    <AlertBanner.Root variant="success">
+      <AlertBanner.Content css={{ minWidth: 250, paddingRight: "$050" }}>
+        <b>Copied: </b>
+        <Box as="span" css={{ fontSize: 16 }}>
+          Import statement for{" "}
+          <Box as="i" css={{ textTransform: "capitalize" }}>
+            {Name}
+          </Box>
+        </Box>
+      </AlertBanner.Content>
+    </AlertBanner.Root>
+  );
 };
 
 export default function Icons({ data }) {
-	const [controlledValue, setControlledValue] = useState("");
+  const [controlledValue, setControlledValue] = useState("");
 
-	useEffect(() => {
-		// if on window and has search query
-		if (typeof window !== "undefined" && window.location.search) {
-			const search = new URLSearchParams(window.location.search).get("search");
-			setControlledValue(search);
-		}
-	}, []);
+  useEffect(() => {
+    // if on window and has search query
+    if (typeof window !== "undefined" && window.location.search) {
+      const search = new URLSearchParams(window.location.search).get("search");
+      setControlledValue(search);
+    }
+  }, []);
 
-	// when search query changes
-	useEffect(() => {
-		if (controlledValue) {
-			// debounce search
-			const timeout = setTimeout(() => {
-				handleChange({ target: { value: controlledValue } });
+  // when search query changes
+  useEffect(() => {
+    if (controlledValue) {
+      // debounce search
+      const timeout = setTimeout(() => {
+        handleChange({ target: { value: controlledValue } });
 
-				// push search query to url
-				if (typeof window !== "undefined") {
-					window.history.pushState(
-						{},
-						"",
-						`${window.location.pathname}?search=${controlledValue}`,
-					);
-				}
-			}, 300);
+        // push search query to url
+        if (typeof window !== "undefined") {
+          window.history.pushState(
+            {},
+            "",
+            `${window.location.pathname}?search=${controlledValue}`
+          );
+        }
+      }, 300);
 
-			return () => clearTimeout(timeout);
-		}
-	}, [controlledValue]);
+      return () => clearTimeout(timeout);
+    }
+  }, [controlledValue]);
 
-	const fuse = new Fuse(data, {
-		keys: ["name", "description"],
-		threshold: 0.1,
-	});
+  const fuse = new Fuse(data, {
+    keys: ["name", "description"],
+    threshold: 0.1,
+  });
 
-	const [ExampleToCopy, setExampleToCopy] = useState(null);
-	const [Name, setName] = useState("");
-	const [Filter, setFilter] = useState([]);
+  const [ExampleToCopy, setExampleToCopy] = useState(null);
+  const [Name, setName] = useState("");
+  const [Filter, setFilter] = useState([]);
 
-	useEffect(() => {
-		if (ExampleToCopy) {
-			window.navigator.clipboard.writeText(ExampleToCopy);
+  useEffect(() => {
+    if (ExampleToCopy) {
+      window.navigator.clipboard.writeText(ExampleToCopy);
 
-			toast(<SuccessToast Name={Name}/>, {
-				position: "top-center",
-				closeButton: false,
-				autoClose: 2000,
-				hideProgressBar: true,
-				draggable: false,
-				onClose: () => {
-					setName(null);
-				},
-			});
-		}
-	}, [ExampleToCopy, Name]);
+      toast(<SuccessToast Name={Name} />, {
+        position: "top-center",
+        closeButton: false,
+        autoClose: 2000,
+        hideProgressBar: true,
+        draggable: false,
+        onClose: () => {
+          setName(null);
+        },
+      });
+    }
+  }, [ExampleToCopy, Name]);
 
-	function setVariables(example, Name) {
-		setName(Name);
-		setExampleToCopy(example);
-	}
+  function setVariables(example, Name) {
+    setName(Name);
+    setExampleToCopy(example);
+  }
 
-	function handleChange(e) {
-		const value = e.target.value;
+  function handleChange(e) {
+    const value = e.target.value;
 
-		setControlledValue(value);
+    setControlledValue(value);
 
-		const result = fuse.search(value);
-		setFilter(result);
-	}
+    const result = fuse.search(value);
+    setFilter(result);
+  }
 
-	const GetIcons = () => {
-		let list;
+  const GetIcons = () => {
+    let list;
 
-		if (Filter.length === 0) {
-			list = Object.keys(AllAssets).filter(
-				(asset) => !logoList.includes(paramCase(asset)),
-			);
-		} else {
-			list = Filter.map((filtered) =>
-				pascalCase(filtered.item.name).replace("15", "Svg15"),
-			);
-		}
+    if (Filter.length === 0) {
+      list = Object.keys(AllAssets).filter(
+        (asset) => !logoList.includes(paramCase(asset))
+      );
+    } else {
+      list = Filter.map((filtered) =>
+        pascalCase(filtered.item.name).replace("15", "Svg15")
+      );
+    }
 
-		return list.map((Asset, i) => {
-			const Sample = AllAssets[Asset];
-			if (!Sample) {
-				return;
-			}
-			const componentName = paramCase(Asset);
+    return list.map((Asset, i) => {
+      const Sample = AllAssets[Asset];
+      if (!Sample) {
+        return;
+      }
+      const componentName = paramCase(Asset);
 
-			const importExample = `import { ${Asset} } from "@washingtonpost/wpds-assets";`;
+      const importExample = `import { ${Asset} } from "@washingtonpost/wpds-assets";`;
 
-			return (
-				<MDXStyling.Cell key={i}>
-					<Box
-						as="button"
-						onClick={() => setVariables(importExample, componentName)}
-						css={{
-							display: "flex",
-							width: "100%",
-							flexDirection: "column",
-							alignItems: "center",
-							justifyContent: "center",
-							cursor: "pointer",
-							gap: "$075",
-							border: "none",
-							"&:hover": { opacity: 0.5 },
-							backgroundColor: theme.colors.gray500,
-							padding: theme.space[100],
-							color: "$primary",
-						}}
-					>
-						<Icon size="$150">
-							<Sample />
-						</Icon>
-						<Box as="span">{componentName}</Box>
-					</Box>
-				</MDXStyling.Cell>
-			);
-		});
-	};
+      return (
+        <MDXStyling.Cell key={i}>
+          <Box
+            as="button"
+            onClick={() => setVariables(importExample, componentName)}
+            css={{
+              display: "flex",
+              width: "100%",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+              gap: "$075",
+              border: "none",
+              "&:hover": { opacity: 0.5 },
+              backgroundColor: theme.colors.gray500,
+              padding: theme.space[100],
+              color: "$primary",
+            }}
+          >
+            <Icon size="$150">
+              <Sample />
+            </Icon>
+            <Box as="span">{componentName}</Box>
+          </Box>
+        </MDXStyling.Cell>
+      );
+    });
+  };
 
-	return (
-		<>
-			<Box css={{ marginBottom: "$050" }}>
-				<InputText
-					onChange={handleChange}
-					label="Search"
-					icon="right"
-					value={controlledValue}
-				>
-					<Icon label="">
-						<Search />
-					</Icon>
-				</InputText>
-			</Box>
+  return (
+    <>
+      <Box css={{ marginBottom: "$050" }}>
+        <InputText
+          onChange={handleChange}
+          label="Search"
+          icon="right"
+          value={controlledValue}
+        >
+          <Icon label="">
+            <Search />
+          </Icon>
+        </InputText>
+      </Box>
 
-			<Grid maxSize={"150px"}>
-				<GetIcons />
-			</Grid>
-		</>
-	);
+      <Grid maxSize={"150px"}>
+        <GetIcons />
+      </Grid>
+    </>
+  );
 }
