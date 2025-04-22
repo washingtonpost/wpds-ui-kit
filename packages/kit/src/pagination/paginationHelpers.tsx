81f038cded15d14b84ecb9008901722eeecd4724
AshTol <ashley.tolen@washpost.com>
2025-04-22 18:20:25 -0400

    feat: fix button transitions with theme transitions in out token [WE-84]

--- packages/kit/src/pagination/paginationHelpers.tsx
+++ packages/kit/src/pagination/paginationHelpers.tsx
@@ -11,7 +11,7 @@ export const StyledButton = styled(Button, {
   fontSize: "$087",
   lineHeight: "16px",
   textDecoration: "none",
-  transition: "background-color .05ms linear",
+  transition: theme.transitions.inOut,
   variants: {
     disabled: {
       true: {
