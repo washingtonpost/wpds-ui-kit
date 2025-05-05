3d51235fb70be5ababccdc235c3d339876e0e6af
Lucio Villa <lucio.villa@washpost.com>
2025-05-05 11:43:15 -0400

    feat: switch to pnpm (#724)

--- packages/kit/src/tabs/TabsRoot.test.tsx
+++ packages/kit/src/tabs/TabsRoot.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { userEvent } from "@testing-library/user-event";
 import { TabsRoot } from "./TabsRoot";
 import { TabsTrigger } from "./TabsTrigger";
 import { TabsList } from "./TabsList";
